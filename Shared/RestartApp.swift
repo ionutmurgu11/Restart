1014ecd505bbd25cd8a16ca066cf235bca4d4bfd
Ionut Murgu <ionutmurgu11@aol.com>
2022-01-19 20:05:37 +0200

    Initial Commit

---
+++ Shared/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Shared
+//
+//  Created by Ionut Murgu on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
